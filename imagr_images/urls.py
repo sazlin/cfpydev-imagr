22d53b730cc836a1b517e10a1cc2fa795bfaa5bf
Sean Azlin <sazlin@gmail.com>
2014-07-10 17:26:29 -0700

    Added super simple user authentication.

--- imagr_images/urls.py
+++ imagr_images/urls.py
@@ -4,5 +4,7 @@ from imagr_images import views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^home/',views.home_page, name='home_page'),
+    url(r'^login/$', views.user_login, name='user_login'),
 )
--- imagr_images/views.py
+++ imagr_images/views.py
@@ -1,10 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, render_to_response
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
 
 
-# Create your views here.
 def index(request):
-    return HttpResponse("You should sign up for Imagr!! It's da bomb.")
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('home_page'))
+
+    return HttpResponseRedirect(reverse('user_login'))
+
+
+def user_login(request):
+    #source: http://www.tangowithdjango.com/book/chapters/login.html
+    # Like before, obtain the context for the user's request.
+    context = RequestContext(request)
+
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Use Django's machinery to attempt to see if the username/password
+        # combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return HttpResponseRedirect('/home/')
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Account Inactive. Bummer :(")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print "Invalid login: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login.")
+
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render_to_response('login.html', {}, context)
+
+
+def home_page(request):
+    return HttpResponse("This is the home_page.")
 
 """
 A "front page" that shows anonymous users something nice to encourage them to sign up (don't worry that we lack a means for them to sign up yet.  We'll add that soon).
